c5e173d6f416668b825d59055c47921b19126991
APP-GASTOS <marianopoliti@gmail.com>
2026-08-10 15:39:47 -0300

    corrijo error de tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import SideBar from './components/SideBar';
+import Sidebar from './components/Sidebar';
 import Dashboard from './pages/DashboardRefact.jsx';
 import Login from './pages/Login';
 import Graficos from './pages/Graficos';
